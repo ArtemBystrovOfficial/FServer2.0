6f17ef5760c92dcfb779cc48b51476f82f45d921
ArtemBystrovOfficial <bystrov.official.one@gmail.com>
2022-09-17 07:20:03 +0800

    add-win

--- build/FServer2_0/lib/sgoogletest/googlemock/CTestTestfile.cmake
+++ build/FServer2_0/lib/sgoogletest/googlemock/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/artem/Documents/ASIO_PRACTICE/FServer2_0/libs/googletest/googlemock
-# Build directory: /home/artem/Documents/ASIO_PRACTICE/build/FServer2_0/lib/sgoogletest/googlemock
+# Source directory: C:/Users/Temas/Documents/repos_vs_code/FServer2.0/FServer2_0/libs/googletest/googlemock
+# Build directory: C:/Users/Temas/Documents/repos_vs_code/FServer2.0/build/FServer2_0/lib/sgoogletest/googlemock
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
