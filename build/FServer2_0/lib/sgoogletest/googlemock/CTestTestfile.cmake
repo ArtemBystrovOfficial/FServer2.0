5c577eda981f886b4fcd40fe7a1993553ffa1e16
Artem <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2022-09-17 15:41:25 +0800

    Delete build directory

--- build/FServer2_0/lib/sgoogletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artem/Documents/ASIO_PRACTICE/FServer2_0/libs/googletest/googlemock
-# Build directory: /home/artem/Documents/ASIO_PRACTICE/build/FServer2_0/lib/sgoogletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/FServer2_0/lib/sgoogletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artem/Documents/ASIO_PRACTICE/FServer2_0/libs/googletest/googletest
-# Build directory: /home/artem/Documents/ASIO_PRACTICE/build/FServer2_0/lib/sgoogletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
